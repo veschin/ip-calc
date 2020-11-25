65db64d9ecac4defff32297a7d5f1f35a53fcb25
Oleg Veschin <unclemadolu2@gmail.com>
2020-11-25 15:15:34 +0700

    core: added prefinal changes

--- src/core.clj
+++ src/core.clj
@@ -1,19 +1,8 @@
 (ns core
   (:require [clojure.string :refer [split join]]))
 
-(def config {:mask "255.255.255.12" :ip "82.179.21.45"})
+(def config {:mask "255.255.255.255" :ip "82.179.21.45"})
 
-;; TODO
-;; 1) IP адрес в десятичной и в двоичной форме +
-;; 2) Префикс маски сети +
-;; 3) Маска сети в десятичной и двоичной форме +
-;; 4) Обратная маска подсети в десятичной и двоичной форме +
-;; 5) IP адрес сети в десятичной и двоичной форме +
-;; 6) Широковещательный адрес в десятичной и двоичной форме +
-;; 7) IP адрес первого хоста в десятичной и двоичной форме
-;; 8) IP адрес последнего хоста в десятичной и двоичной форме
-;; 9) Количество доступных адресов
-;; 10) Количество рабочих адресов для хостов
 (defn str10->str2
   [string-with-10-base]
   (if (int? string-with-10-base)
@@ -23,6 +12,10 @@
 (defn address-to-binary
   [address]
   (map str10->str2 address))
+
+(defn address-to-binary*
+  [address]
+  (join "." (map str10->str2 address)))
 
 (defn address-to-nums
   [address]
@@ -56,11 +49,24 @@
   (if (zero? (last address)) (conj (pop (vec address)) 255) address))
 
 (defn get-first-n-last-hosts
-  [broadcast-address])
+  [broadcast-address]
+  (let [last-part (last broadcast-address)
+        first-n-last (if (= 255 last-part)
+                       [1 256]
+                       [(dec last-part) (inc last-part)])]
+    (map #(conj (pop (vec broadcast-address)) %) first-n-last)))
 
 (let [[splitted-mask splitted-ip] (->> (vals config) (map #(split % #"\.")) (map address-to-nums))
       mask-prefix (get-mask-prefix splitted-mask)
       network-address (get-network-ip splitted-ip splitted-mask)
       wildcard-mask (get-wildcard-mask splitted-mask)
-      broadcast-address (get-broadcast-address network-address)]
-  broadcast-address)
+      broadcast-address (get-broadcast-address network-address)
+      [min-address max-address] (get-first-n-last-hosts broadcast-address)]
+  {:ip [splitted-ip (address-to-binary* splitted-ip)]
+   :mask [splitted-mask (address-to-binary* splitted-mask)]
+   :mask-prefix mask-prefix
+   :network-address [network-address (address-to-binary* network-address)]
+   :wildcard-mask [wildcard-mask (address-to-binary* wildcard-mask)]
+   :broadcast-address [broadcast-address (address-to-binary* broadcast-address)]
+   :min-address [min-address (address-to-binary* min-address)]
+   :max-address [max-address (address-to-binary* max-address)]})
